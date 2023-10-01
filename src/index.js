ff9e162b0cdde3c2262880eb41d897418bc8b252
kunaldec022002 <kunalingale72@gmail.com>
2023-10-01 16:57:36 +0530

    add react routing

--- src/index.js
+++ src/index.js
@@ -1,17 +1,45 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { RouterProvider,createBrowserRouter } from 'react-router-dom';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import About from './Views/About/about';
+import Contact from"./Views/Contact/contact";
+import Home from"./Views/Home/home";
+
+
+
+const router =createBrowserRouter(
+  [
+        {
+          path:'/',
+          element:<Home/>
+        },
+        {
+          path:'/About',
+          element:<About/>
+        },
+        {
+          path:'/Contact',
+          element: <Contact/>
+        }
+  ]
+  );
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+
+
+
+
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  
+  <>
+  
+    <RouterProvider router={router}/>
+
+    
+   
+  </>
+
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
